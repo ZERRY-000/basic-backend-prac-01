11a09001a7fe62b0820c1f4072193f30ebc243e9
Kritsada Chan-inn <bankth99@gmail.com>
2025-07-08 14:31:32 +0700

    add CRUD application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 const app = document.getElementById('app');
+const homepage = document.getElementById('homepage');
 
 app.innerHTML = `
     <h1 id="header_auth">Login</h1>
     <div><input id="username_input" type="text" placeholder="username"></div>
-    <div><input id="password_input" type="text" placeholder="password"></div>
+    <div><input id="password_input" type="password" placeholder="password"></div>
     <div>
       <button id="register_btn">sign up</button>
       <button id="login_btn">sign in</button>
@@ -21,7 +22,14 @@ const username_input = document.getElementById('username_input');
 const password_input = document.getElementById('password_input');
 let switch_auth_login = true;
 
+// homepage element
+const topic_input = document.getElementById('topic_input');
+const content_input = document.getElementById('content_input');
+const add_todo = document.getElementById('add_todo')
+const todo_list = document.getElementById('todo_list');
+
 register_btn.style.display = 'none';
+homepage.style.display = 'none';
 
 const switch_auth = () => {
   switch_auth_login = !switch_auth_login;
@@ -41,6 +49,7 @@ const switch_auth = () => {
 
 console.log(username_input.value);
 console.log(password_input.value);
+
 async function register_func() {
   try {
     const response = await fetch('/auth/register', {
@@ -63,5 +72,126 @@ async function register_func() {
 
 }
 
+let user_id;
+
+async function login_func() {
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({
+        username: username_input.value,
+        password: password_input.value
+      })
+    });
+
+    const data = await response.json();
+    if(data.message === 'ok') {
+      user_id = data.user_id;
+      homepage_display();
+    }
+    else {
+      user_id = null;
+      alert(data.message);
+    }
+    console.log('data (print from index.js):',data);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+
+}
+
+async function todos_fetch() {
+  try {
+    const response = await fetch(`/todos/user/${user_id}`);
+
+    const data = await response.json();
+    // console.log("data (from todos_fetch index.js):",data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function create_todo() {
+  try {
+    const response = await fetch('/todos/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: user_id,
+        topic: topic_input.value,
+        content: content_input.value,
+      })
+    })
+    const data = await response.json();
+    createItem(data.todo_instance.topic,data.todo_instance.content,data.todo_instance._id, data.todo_instance.completed);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function deleteItem(todoId) {
+  try {
+    const response = await fetch(`/todos/delete/${todoId}`, {
+      method: 'DELETE'
+    }) 
+    const data = await response.json();
+    const deleteElement = document.getElementById(`${data.id}`);
+    deleteElement.remove();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function doneItem(todoId) {
+  const item = document.getElementById(`${todoId}`);
+  if(item.classList.contains('completed')) return;
+  try {
+    const response = await fetch(`/todos/done/${todoId}`, {
+      method: 'PUT'
+    }) 
+    const data = await response.json();
+    const doneElement = document.getElementById(`${todoId}`);
+    doneElement.classList.add("completed");
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function createItem(topic, content, todoId, completed) {
+  console.log(todoId);
+  const todo = document.createElement("div");
+  todo.setAttribute('id', todoId);
+  todo.classList.add("todo-item");
+  if(completed) todo.classList.add("completed");
+  todo.innerHTML = `
+  <h3>${topic}</h3>
+  <div>${content}</div>
+  <button onclick="deleteItem('${todoId}')">delete</button>
+  <button onclick="doneItem('${todoId}')">done</button>
+  `
+  todo_list.appendChild(todo);
+}
+
+
+async function homepage_display() {
+  app.style.display = 'none';
+  homepage.style.display = '';
+  const todos = await todos_fetch();
+
+  // console.log("todos: ", todos);
+  for(let i = 0; i < todos.length; ++i){
+    createItem(todos[i].topic, todos[i].content, todos[i]._id, todos[i].completed);
+  }
+}
+
 switch_auth_button.addEventListener('click', switch_auth);
 register_btn.addEventListener('click', register_func);
+login_btn.addEventListener('click', login_func);
+add_todo.addEventListener('click', create_todo);
